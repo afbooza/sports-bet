771a034cc9d03403fe3b4a7b40949272470bf10d
Antonio <busaaf@gmail.com>
2019-05-24 23:05:43 -0400

    first commit

---
+++ api.js
@@ -0,0 +1,15 @@
+const http = require('http');
+
+http.get({
+    hostname: 'localhost',
+    port: 80,
+    path: '/',
+    agent: false  // create a new agent just for this one request
+  }, (res) => {
+    // Do stuff with response
+  });
+
+const keepAliveAgent = new http.Agent({ keepAlive: true });
+options.agent = keepAliveAgent;
+http.request(options, onResponseCallback);
+
